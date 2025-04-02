ff9a7d5e1b08c149cc80d62f6c2c2d0c3ec23122
NET_12_NguyenQuangKhanhAn <annqkhe180905@fpt.edu.vn>
2025-04-02 16:55:42 +0700

    patient payment not done yet

--- FindingHealthcareSystem/Pages/Patient/Payment.cshtml.cs
+++ FindingHealthcareSystem/Pages/Patient/Payment.cshtml.cs
@@ -1,12 +1,27 @@
+using BusinessObjects.DTOs.Payment;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Interfaces;
 
 namespace FindingHealthcareSystem.Pages.Patient
 {
     public class PaymentModel : PageModel
     {
-        public void OnGet()
+        private readonly IPaymentService _paymentService;
+
+        public PaymentModel(IPaymentService paymentService)
         {
+            _paymentService = paymentService;
+        }
+
+        public List<PaymentDto> PatientPayments { get; set; }
+
+        [BindProperty]
+        public int UserId { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            PatientPayments = await _paymentService.GetPaymentsByPatientIdAsync(UserId);
         }
     }
 }
